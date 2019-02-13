c88a1f81a375f6249309b13d23143a0403df0f20
Shashidhar <shashikdmn@gmail.com>
2019-02-13 16:55:20 +0530

    Program without exception handler

---
+++ p4a.py
@@ -0,0 +1,13 @@
+#Program to demonstrate exception
+
+v1_str = input("Enter value for v1 = ");
+v2_str = input("Enter value for v2 = ");
+
+v1_int = int(v1_str)
+v2_int = int(v2_str)
+
+sum = v1_int + v2_int;
+print("Sum of "+ v1_str + " and " + v2_str + " is :" + str(sum));
+
+#Enter alphabet values for v1 or v2, the program with throw exception.
+#Solution is in p4b.py
